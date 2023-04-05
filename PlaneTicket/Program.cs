79c6d4d0d1898cbf4f778a574c8159ab8ded2166
NadhifFauzilA <Nadhiffauzil@students.amikom.ac.id>
2023-04-05 17:35:21 +0700

    Add project files.

---
+++ PlaneTicket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneTicket
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            planeticket ticket1 = new planeticket();
+            planeticket ticket2 = new planeticket();
+
+            ticket1.Origin = "Jogja";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            ticket2.Origin = "Jakarta";
+            ticket2.Destination = "IND";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PlaneTicket/planeticket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneTicket
+{
+    public class planeticket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+
+        public void PrintSummary ()
+        {
+            Console.WriteLine("Origin {0} ", Origin);
+            Console.WriteLine("Destination {0}", Destination);
+            Console.WriteLine("Cost {0} ", Cost); 
+
+            if (Currency == "USD" ) {
+                Console.WriteLine("Convert to Rupiah {0} ", Cost * 150000);
+                    }
+            else if (Currency == "IDR")
+            {
+                Console.WriteLine("Convert to USD {0} ", Cost / 100);
+            }
+        }
+    }
+}
